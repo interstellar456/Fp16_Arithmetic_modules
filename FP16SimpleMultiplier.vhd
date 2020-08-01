5a2ce639531f976b3cb5ca62473c9c3f11da1170
Kinshuk Srivastava <66518554+interstellar456@users.noreply.github.com>
2020-08-01 13:57:12 +0530

    working nicely now, cant handle exceptions though.
    tested for num1 = num2 = 1.548828125
    expected answer = 2.3716..
    answer by hardware = 2.3984375

--- FP16SimpleMultiplier.vhd
+++ FP16SimpleMultiplier.vhd
@@ -41,66 +41,56 @@ entity FP16SimpleMultiplier is
 end FP16SimpleMultiplier;
 
 architecture Behavioral of FP16SimpleMultiplier is
-    signal num1 : std_logic_vector(10 downto 0) := (others => '0') ;
-    signal num2 : std_logic_vector(10 downto 0) := (others => '0');
-    signal exp1 : std_logic_vector(4 downto 0) := (others => '0');
-    signal exp2 : std_logic_vector(4 downto 0) := (others => '0');
-    signal expsum : std_logic_vector(4 downto 0) := (others => '0');
-    signal tempreg : std_logic_vector(11 downto 0):= (others => '0');
-    signal multi: std_logic_vector(21 downto 0) := (others => '0');
-    signal s1,s2,sz : std_logic := '0';
-    signal round: std_logic_vector(9 downto 0) := (others => '0');
 begin
     process
-    variable xt : bit := '0';
-    variable branch1 : bit := '0';
-    variable branch2 : bit := '0';
-    variable branch3 : bit := '0';
-    variable branch4 : bit := '0';
+    variable num1 : std_logic_vector(10 downto 0) := (others => '0') ;
+    variable num2 : std_logic_vector(10 downto 0) := (others => '0');
+    variable exp1 : std_logic_vector(4 downto 0) := (others => '0');
+    variable exp2 : std_logic_vector(4 downto 0) := (others => '0');
+    variable expsum : std_logic_vector(4 downto 0) := (others => '0');
+    variable tempreg : std_logic_vector(15 downto 0):= (others => '0');
+    variable multi: std_logic_vector(21 downto 0) := (others => '0');
+    variable mrounded : std_logic_vector(11 downto 0) := (others => '0');
+    variable s1,s2,sz,ready : std_logic := '0';
+    variable round: std_logic_vector(9 downto 0) := (others => '0');
         begin
         wait until rising_edge(clk);
-            num1(9 downto 0) <= numi1(9 downto 0);
-            num2(9 downto 0) <= numi2(9 downto 0);
-            num1(10) <= '1';
-            num2(10) <= '1';
-            exp1 <= numi1(14 downto 10);
-            exp2 <= numi2(14 downto 10);
-            s1 <= numi1(15);
-            s2 <= numi2(15);
-            xt:= '1';
-            if(xt = '1') then
-                sz <= s1 xor s2;
-                expsum <= std_logic_vector(unsigned(exp1) + unsigned(exp2) - to_unsigned(2#01111#,5));
-                multi <= std_logic_vector(unsigned(num1) * unsigned(num2));
-                branch1 := '1';
-                if(branch1 = '1') then
-                    round <= multi(9 downto 0);
-                    branch2 := '1';
-                    if(branch2 = '1') then
-                        case to_integer(unsigned(round)) is
-                            when 0 to 512 =>
-                                tempreg <= multi(21 downto 10);
-                                 if(sz = '1') then
-                                    tempreg <= std_logic_vector(unsigned(tempreg) + to_unsigned(1,12));
-                                end if;
-                            when 513 to 1024 => 
-                                tempreg <= multi(21 downto 10);
-                                if(sz = '0') then
-                                    tempreg <= std_logic_vector(unsigned(tempreg) + to_unsigned(1,12));
-                                end if;
-                            when others => tempreg <= tempreg;
-                                end case;
-                             if(unsigned(tempreg) >= to_unsigned(2#011111111111#,12)) then
-                                   tempreg <= std_logic_vector(shift_right(unsigned(tempreg),1));
-                                   expsum <= std_logic_vector(unsigned(expsum) + to_unsigned(1,5));
-                                end if;
-                            end if;
-                        branch1 := '0';
-                        end if;
-                    xt := '0';
+            ans <= std_logic_vector(to_unsigned(2#0000000000000000#,16));
+            num1(9 downto 0) := numi1(9 downto 0);
+            num1(10) := '1';
+            num2(9 downto 0) := numi2(9 downto 0);
+            num2(10) := '1';
+            exp1 := numi1(14 downto 10);
+            exp2 := numi2(14 downto 10);
+            s1 := numi1(15);
+            s2 := numi2(15);
+            sz := s1 xor s2;
+            expsum := std_logic_vector(unsigned(exp1) + unsigned(exp1) - to_unsigned(2#01111#,5));
+            multi := std_logic_vector(unsigned(num1)*unsigned(num2));
+            mrounded := multi(21 downto 10);
+            round := multi(9 downto 0);
+            case to_integer(unsigned(round)) is
+                when 0 to 512 =>
+                    if(sz = '1') then
+                        mrounded := std_logic_vector(unsigned(mrounded) + to_unsigned(1,12));
                     end if;
-               ans(15) <= std_logic(sz);
-               ans(14 downto 10) <= expsum;
-               ans(9 downto 0) <= tempreg(9 downto 0);
+                when 513 to 1023 => 
+                    if(sz = '0') then
+                        mrounded := std_logic_vector(unsigned(mrounded) + to_unsigned(1,12));
+                    end if;
+                when others =>
+                    assert ready = '0';
+            end case;
+            if(mrounded(11) = '1') then
+                mrounded := std_logic_vector(shift_right(unsigned(mrounded),1));
+                expsum := std_logic_vector(unsigned(expsum) + to_unsigned(1,5));
+            end if;
+            tempreg(9 downto 0) := mrounded(9 downto 0);
+            tempreg(14 downto 10) := expsum(4 downto 0);
+            tempreg(15) := sz;
+            ready := '1';
+            if(ready = '1') then
+                ans <= tempreg;
+            end if;
         end process;
 end Behavioral;
